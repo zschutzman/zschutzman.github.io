bf095e5471786cf023e6be854523cfdb45fccd50
Zachary Schutzman <zach@schutzman.net>
2018-07-15 16:01:37 -0400

    we have clicking!

--- metagraph/force.js
+++ metagraph/force.js
@@ -4,10 +4,8 @@ var w = Math.round(.35*document.documentElement.clientWidth)
     wp = Math.round(.20*document.documentElement.clientWidth);
 
     
-   
-    
-    
-
+var toggle = 0;
+ 
 var vis = d3.select("#chart1")
   .append("svg")
     .attr("width", w+wp)
@@ -19,7 +17,7 @@ var tooltip = d3.select("body")
 	.style("z-index", "10")
 	.style("opacity", 0);
 	
-    
+
 
  
 // Define the div for the tooltip
@@ -46,7 +44,9 @@ d3.json("gr.json", function(json) {
       .attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+      .attr("y2", function(d) { return d.target.y; })
+      .attr("u",function(d) {return d3.select(d.source);})
+      .attr("v", function(d) {return d.target;});
 
   var node = vis.selectAll("circle.node")
       .data(json.nodes)
@@ -57,6 +57,8 @@ d3.json("gr.json", function(json) {
       .attr("type", function(d) {return d.Type;})
       .attr("html_rep", function(d) {return d.html_rep;})
       .attr("r", function(d) {return Math.round(2*d.deg);})
+    
+      .style("opacity", 1.)
       .style("fill", function(d) { if (d.Type == 20) return 'PapayaWhip'; if (d.Type == 21) return 'Gold'; return fill(d.Type); })
       .call(force.drag)
       .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
@@ -71,7 +73,7 @@ d3.json("gr.json", function(json) {
         
         tooltip.transition()		
         .duration(200)		
-        .style("opacity",.9);		
+        .style("opacity",.95);		
 
 
      
@@ -110,23 +112,38 @@ d3.json("gr.json", function(json) {
                 .style("opacity", 0);	            
      })
       
-      
+     /* 
     .on("click", function(){
-        var c = d3.select(this);
-        var c2 = this
+
         var x = d3.select(this).attr("cx");
-        var y = d3.select(this).attr("cy");
+        var y = d3.select(this).attr("cy");       
+
+        vis.selectAll("circle.node").each(function(d){
+            d3.select(this).style("opacity",.6);});
+  
+
+        
+        d3.select(this).style("opacity",1.);
+        
+        
+        
         vis.selectAll("line.link").each(function(d){
             var u1 = d3.select(this).attr("x1");
             var u2 = d3.select(this).attr("x2");
             var v1 = d3.select(this).attr("y1");
             var v2 = d3.select(this).attr("y2");
-            d3.select(this).attr("stroke-width", function(d){if ((u1 == x || u2 == x) && (v1==y || v2 == y)) return 8; return Math.sqrt(d.value);})
-            d3.select(this).attr("stroke", function(d){if ((u1 == x || u2 == x) && (v1==y || v2 == y)) return "#000"; return "#555";});
+            d3.select(this).attr("stroke-width", function(d){if ((u1 == x || u2 == x) && (v1==y || v2 == y)) return 8;  return Math.sqrt(d.value);});
+            d3.select(this).attr("stroke", function(d){  if ((u1 == x || u2 == x) && (v1==y || v2 == y)) return "#000"; return "#555";});
+            
 
+
+         
+
+            
         });
-    });
-
+    
+    })*/
+    .on("click", connectedNodes);
     
 
       
@@ -147,7 +164,38 @@ d3.json("gr.json", function(json) {
     node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
   });
+  
+  
+ var linkedByIndex = {};
+vis.selectAll("line.link").each(function(d){
+     linkedByIndex[d.source.index + "," + d.target.index] = 1;
 });
+
+
+function neighboring(a, b) {
+    return linkedByIndex[a.index + "," + b.index];
+}
+
+function connectedNodes() {
+
+    if (toggle == 0) {
+        d = d3.select(this).node().__data__;
+        node.style("opacity", function (o) {
+            return neighboring(d, o) | neighboring(o, d) | o === d ? 1 : 0.5;
+        });
+        toggle = 1;
+    } else {
+        node.style("opacity", 1);;
+        toggle = 0;
+    }
+
+} 
+  
+  
+  
+  
+});
+
 
 
 
